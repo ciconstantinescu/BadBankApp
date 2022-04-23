f452a73f98b2c13703fde7af22202bd60c9c279b
Carmen <carmen.constantinescu@urbanteachers.org>
2022-04-23 15:17:25 -0400

    commit

--- index.js
+++ index.js
@@ -102,6 +102,6 @@ app.get('/account/all', function (req, res) {
         });
 })
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Running on port: ' + port);
